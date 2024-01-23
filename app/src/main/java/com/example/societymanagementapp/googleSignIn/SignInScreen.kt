fc6c39e17bcb120ca62f655018657009a949901b
21Neutron <mayankbisht2113@gmail.com>
2024-01-23 16:16:39 +0530

    Revert "logoupdate"
    This reverts commit 7b84a81dc3543ff43c8408d29263de6f54900149.

--- app/src/main/java/com/example/societymanagementapp/googleSignIn/SignInScreen.kt
+++ app/src/main/java/com/example/societymanagementapp/googleSignIn/SignInScreen.kt
@@ -2,23 +2,16 @@ package com.example.societymanagementapp.googleSignIn
 
 import android.widget.Button
 import android.widget.Toast
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.societymanagementapp.R
 import kotlinx.coroutines.Job
 
 @Composable
@@ -39,16 +32,8 @@ fun SignInScreen(
         .padding(16.dp),
         contentAlignment = androidx.compose.ui.Alignment.Center
     ){
-        Button(
-            modifier = Modifier.size(100.dp),
-            onClick = { onSignInClick },
-            shape = CircleShape,
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Transparent,
-                contentColor = Color.Transparent
-            )
-        ) {
-            Image(painterResource(id = R.drawable.googleicon), contentDescription = "sign in logo")
+        Button(onClick = onSignInClick) {
+            Text(text ="Sign In")
         }
     }
 }
