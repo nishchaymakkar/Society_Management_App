a1cd3cf116f3d8afec25395678da97c9834095e7
21Neutron <mayankbisht2113@gmail.com>
2024-01-23 19:02:52 +0530

    login image upadted

--- app/src/main/java/com/example/societymanagementapp/googleSignIn/SignInScreen.kt
+++ app/src/main/java/com/example/societymanagementapp/googleSignIn/SignInScreen.kt
@@ -2,17 +2,25 @@ package com.example.societymanagementapp.googleSignIn
 
 import android.widget.Button
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.societymanagementapp.R
 import kotlinx.coroutines.Job
+
 
 @Composable
 fun SignInScreen(
@@ -20,20 +28,32 @@ fun SignInScreen(
     onSignInClick: () -> Unit
 ) {
     val context = LocalContext.current
-    LaunchedEffect(key1 = state.signInError){
+    LaunchedEffect(key1 = state.signInError) {
         state.signInError?.let { error ->
             Toast.makeText(
-                context,error,Toast.LENGTH_LONG
+                context, error, Toast.LENGTH_LONG
             ).show()
         }
     }
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp),
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         contentAlignment = androidx.compose.ui.Alignment.Center
-    ){
-        Button(onClick = onSignInClick) {
-            Text(text ="Sign In")
+    ) {
+        Button(
+            onClick = onSignInClick,
+            modifier = Modifier.size(100.dp),
+            shape = CircleShape,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Transparent
+            )
+        ) {
+            Image(
+                painterResource(id = R.drawable.googleicon),
+                contentDescription = "sign in logo "
+            )
         }
     }
 }
