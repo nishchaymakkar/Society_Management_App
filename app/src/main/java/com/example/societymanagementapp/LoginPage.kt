7602d52e2ebeacc0c154a30b6beb2cb14c1490d3
nishchaymakkar <nishchaymakkar5@gmail.com>
2024-01-26 01:31:12 +0530

    Login Page UI

--- app/src/main/java/com/example/societymanagementapp/LoginPage.kt
+++ app/src/main/java/com/example/societymanagementapp/LoginPage.kt
@@ -1,12 +1,14 @@
 package com.example.societymanagementapp
 
 import android.widget.ImageButton
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,10 +17,15 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.contentColorFor
@@ -35,28 +42,31 @@ import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Preview
 @Composable
 fun loginPage() {
 
     Box(modifier = Modifier
         .fillMaxSize()
-        .background(Color.White),
+        .background(Color.White)
     ){
-        Column(modifier = Modifier.fillMaxSize()) {
-
-
+        Column(modifier = Modifier
+            .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(150.dp)
-                    .background(Color.White),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .background(Color.White),
                 verticalAlignment = Alignment.Top,
                 horizontalArrangement = Arrangement.Center
             ) {
@@ -69,89 +79,96 @@ fun loginPage() {
                         .align(alignment = Alignment.CenterVertically)
                 )
             }
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.White)
-                    .padding(10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-            ) {
-                //for username
+
                 var username by remember {
-                    mutableStateOf("" )
+                    mutableStateOf("")
                 }
-                TextField(
+                OutlinedTextField(
                     value = username,
-                    onValueChange ={ username = it },
+                    onValueChange = { username = it },
+                    maxLines = 1,
                     textStyle = LocalTextStyle.current.copy(
                         textAlign = TextAlign.Left,
                         fontSize = 20.sp,
                         color = Color.Black,
-                        background = Color.White,
-                    ),
+                        ),
 
-                    placeholder = {
-                        Text(text = "Username",
-                            color = Color.DarkGray,
-                            modifier = Modifier.background(Color.White),
+                    label = {
+                        Text(
+                            text = "Username"
                         )
 
                     },
                     leadingIcon = {
-                        Image(painterResource(id = R.drawable.person)  , contentDescription ="person",
-                            Modifier.background(Color.White))
-                    },
+                        Image(
+                            painterResource(id = R.drawable.person), contentDescription = "person"
+                        )
+                    }
                 )
-                //for password
+            Spacer(modifier = Modifier.height(10.dp))
                 var password by remember {
-                    mutableStateOf("" )
+                    mutableStateOf("")
                 }
-                TextField(
+                OutlinedTextField(
                     value = password,
-                    onValueChange ={ password = it },
+                    onValueChange = { password = it },
+                    maxLines = 1,
                     textStyle = LocalTextStyle.current.copy(
                         textAlign = TextAlign.Left,
                         fontSize = 20.sp,
                         color = Color.Black,
-                        background = Color.White,
                     ),
-
-                    placeholder = {
-                        Text(text = "password",
-                            color = Color.DarkGray,
-                            modifier = Modifier.background(Color.White),
+                    label = {
+                        Text(
+                            text = "Password"
                         )
 
                     },
                     leadingIcon = {
-                        Image(painterResource(id = R.drawable.password)  , contentDescription ="password",
-                            Modifier.background(Color.White))
-                    },
-                )
-            }
+                        Image(
+                            painterResource(id = R.drawable.password), contentDescription = "person"
+                        )
+                    }
 
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(150.dp)
-                    .background(Color.White),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+                )
+            Spacer(modifier = Modifier.height(30.dp))
+            OutlinedButton(onClick = { /*TODO*/ },
+                border= BorderStroke(1.dp, Color.Blue),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White,
+                    contentColor = Color.Blue,
+                )
             ) {
-                Text(text = "or sign in with",
-                    fontSize = 20.sp)
-                Button(
-                    modifier = Modifier.size(100.dp),
-                    onClick = { /* todo*/ },
-                    shape = CircleShape,
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Transparent,
-                        contentColor = Color.Transparent
+                Text(text ="Login")
+            }
+            Spacer(modifier = Modifier.height(5.dp))
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "forgot password?",
+                    color = Color.Black,
+                    fontSize = 10.sp,
+                    fontWeight = FontWeight.Bold,
                     )
-                ) {
-                    Image(painterResource(id = R.drawable.googleicon), contentDescription = "sign in logo")
-                }
+            }
+            Spacer(modifier = Modifier.height(70.dp))
+            Text(text = "or login with",
+                color = Color.Black,
+                fontSize = 10.sp,
+                fontWeight = FontWeight.Bold,
+                )
+            Spacer(modifier = Modifier.height(5.dp))
+            Button(
+                onClick = {/*todo*/},
+                modifier = Modifier.size(100.dp),
+                shape = CircleShape,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Color.Transparent
+                )
+            ) {
+                Image(
+                    painterResource(id = R.drawable.googleicon),
+                    contentDescription = "sign in logo "
+                )
             }
         }
     }
