b97a2cb56cb454bd854d4f37428c6327151fbe25
nishchaymakkar <nishchaymakkar5@gmail.com>
2024-02-18 22:02:23 +0530

    Add visitor card

--- app/src/main/java/com/example/societymanagementapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/societymanagementapp/ui/theme/Color.kt
@@ -9,5 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
-
+val darkGreen = Color(0xFF014D08)
 val offWhite = Color(0xFFE9E9E9)
+
